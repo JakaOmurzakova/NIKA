dd67457d114902585989fd3e377a6c4dccfd6082
JakaOmurzakova <angelina_18_89@mail.ru>
2023-06-27 16:00:44 +0600

    json

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const API = "http://localhost:8000/katalog";
 
+const card = document.querySelector(".card");
+
+async function render() {
+  const res = await fetch(API);
+  const data = res.json();
+
+  console.log(data);
+}
+
+render();
