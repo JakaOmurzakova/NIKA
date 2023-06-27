9fa8a26ed17fc35f532cd6240e664c6da5ddf623
JakaOmurzakova <angelina_18_89@mail.ru>
2023-06-27 20:42:31 +0600

    function paint

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 const API = "http://localhost:8000/katalog";
 
-const card = document.querySelector(".card");
+const card = document.querySelector(".main-card");
 
 async function render() {
   const res = await fetch(API);
   const data = await res.json();
 
-  console.log(data);
+  return data;
 }
 
-render();
+async function paintCard() {
+  const data = await render();
+
+  console.log(data);
+  card.innerText = "";
+
+  data.forEach((item) => {
+    card.innerHTML += `<div class="card">
+    <div class="card-image"><img src="${item.photo}" alt=""></div>
+    <p class="card-title">${item.title}</p>
+    <p class="card-body">
+      ${item.description}
+    </p>
+    <p class="card-price">${item.price} ₽</p>
+  </div>`;
+  });
+}
+
+paintCard();
